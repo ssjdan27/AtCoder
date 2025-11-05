5d3b03792e4a1cb61cdcd2f691bc4c2d8682e3be
Daniel Garza <danielg272313@gmail.com>
2025-11-05 14:21:45 -0700

    solve ABC 430 A

---
+++ ABC 430/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if(c >= a && d < b){
+        cout << "Yes\n";
+        return 0;
+    }
+    cout << "No\n";
+    return 0;
+}
